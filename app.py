9fcba7a5371a97912bc774f7e3336200f20ab68f
gengqirui050118 <160450763+gengqirui050118@users.noreply.github.com>
2024-02-19 12:00:21 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flaskimport flask,request,render_template
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET","post"])
+
+def index():
+  return(render_template("index.html")
+
+if __name__=="__main__":
+  app.run()      
