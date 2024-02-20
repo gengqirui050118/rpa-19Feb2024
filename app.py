c42c261742baa72372ef48f860cc57fef1bffe1e
gengqirui050118 <160450763+gengqirui050118@users.noreply.github.com>
2024-02-20 15:07:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 
 def main():
-r = request.form.get("name")
+r = request.form.get("r")
 return(render_template("main.html",r=r))
 
+@app.route("/imageGPT",methods=["GET", "POST"])
+def main():
+  return(render_templats("imageGPT.html",r=r))
+  
 if __name__=="__main__":
   app.run()      
