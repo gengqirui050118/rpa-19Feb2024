3fdb9365f039936e5a4c32ddbd0e88ca657a9f81
gengqirui050118 <160450763+gengqirui050118@users.noreply.github.com>
2024-02-21 09:07:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 
-app = Flask(__name__) 
-
+app = Flask(__name__)
 r = ""
 first_time = 1
 
-@app.route("/",methods["GET","POST"]) 
-def index(): 
-    return(render_template("index.html")) 
 
-@app.route("/main”,methods=["GET","POST"] 
-def  main(): 
-     global r, first_time 
-     if first_time==1: 
-     r = request.form.get("r") 
-     first_time=0 
- return(render_template("main.htm1",r=r)) 
+@app.route("/",methods=["GET","POST"])
+def index():
+  return(render_template("index.html"))
 
-@app.route("/image_gpt”,methods["GET,"POST"]) 
-def image_gpt(): 
-    return(render_template("image_gpt.htm1"))   
+@app.route("/main",methods=["GET","POST"])
+def main():
+  global r,first_time
+  if first_time==1:
+    r = request.form.get("r")
+    first_time = 0
+  return(render_template("main.html",r=r))
+  
+@app.route("/image_gpt",methods=["GET","POST"])
+def image_gpt():
+  return(render_template("image_gpt.html"))
+
+@app.route("/end",methods=["GET","POST"])
+def end():
+  global first_time 
+  first_time = 1
+  return(render_template("end.html"))
+
+if __name__=="__main__":
+  app.run()
+
+
+    
